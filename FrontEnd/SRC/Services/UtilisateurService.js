47ebc0ce7c11ce92879674946c16178b415aec1d
Doryan <122481253+AtourTerelle@users.noreply.github.com>
2025-02-07 21:32:35 +0100

    Ajout page Admin_Home pour connexion

--- FrontEnd/SRC/Services/UtilisateurService.js
+++ FrontEnd/SRC/Services/UtilisateurService.js
@@ -1,16 +1,24 @@
-import Config from 'react-native-config'
-
-
 class UtilisateurService {
 
     constructor(httpClient){
-        this.httpClient = httpClient
+        this.httpClient = httpClient;
     }
 
     async connexionUtilisateur(body){
         
         try{
             const response = await this.httpClient.postConnexion(process.env.EXPO_PUBLIC_URL_API+'login', body)
+            return response.json()
+        } catch(error){
+            console.log("Erreur service : "+ error)
+            throw error
+        }
+    }
+
+    async GetAllUtilisateur(){
+        
+        try{
+            const response = await this.httpClient.get(process.env.EXPO_PUBLIC_URL_API+'getAllUtilisateurs')
             return response.json()
         } catch(error){
             console.log("Erreur service : "+ error)
